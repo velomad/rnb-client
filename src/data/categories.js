f243623eeaa4adfe15f36a878edf9f8c51f12d5f
velomad <48247308+velomad@users.noreply.github.com>
2020-10-29 11:18:40 +0530

    test

---
+++ src/data/categories.js
@@ -0,0 +1,6 @@
+module.exports = {
+	categoyName: [
+        "camea",
+        "mobile"
+    ],
+};
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,72 +1,12 @@
+import cat from "../../data/categories"
+
 const LandingPage = () => {
+
+    console.log(cat.categoyName)
+
     return (
         <div>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
-            <p>
-            epic    
-            </p>
+         test    sda s
         </div>
     )
 }
