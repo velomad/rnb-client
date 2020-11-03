a6b202d8c9d400bcad5338698858622d1601d37d
A00079 <bajay0007@gmail.com>
2020-11-04 04:08:46 +0530

    search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,14 +8,12 @@ import Select from '@material-ui/core/Select';
 import axios from "axios";
 const useStyles = makeStyles((theme) => ({
     formControl: {
-
-        margin: theme.spacing(1),
-        marginRight: theme.spacing(0),
+        position: 'absolute',
+        left: '1px',
+        top: '0px',
         minWidth: 120,
         background: '#fff',
-        borderRadius: '4px',
-        borderTopRightRadius: '0px',
-        borderBottomRightRadius: '0px'
+        borderRadius: '40px',
     },
     searchControl: {
         background: '#fff',
@@ -55,7 +53,7 @@ export default function Filter() {
     const [inputSearch, setInputSearch] = React.useState("");
     const [options, setOptions] = React.useState([]);
 
-    const [unit, setUnit] = React.useState('https://reachnbuy.herokuapp.com/api/v1/search');
+    const [unit, setUnit] = React.useState('http://localhost:8080/api/v1/search');
 
     const debounceOnChange = React.useCallback(
         debounce(value => {
@@ -96,63 +94,33 @@ export default function Filter() {
     return (
         <React.Fragment>
             <div class="flex">
-                <div class="flex-initial text-gray-700 text-center px-0 py-0 m-2 mr-0">
-                    <FormControl variant="filled" className={classes.formControl}>
-                        {/* <InputLabel htmlFor="filled-age-native-simple">Filter</InputLabel> */}
-                        <Select
-                            native
-                            value={unit}
-                            onChange={handleChange}
-                            inputProps={{
-                                name: 'age',
-                                id: 'filled-age-native-simple',
-                            }}
-                        >
-                            {/* <option aria-label="None" value="">All</option> */}
-                            <option selected value={'https://reachnbuy.herokuapp.com/api/v1/search'}>Clothing</option>
-                            <option value={2}>Electronics</option>
-                        </Select>
-                    </FormControl>
+                <div class="flex-initial text-gray-700 text-center  px-0 py-0 m-2 mr-0">
                 </div>
-                <div class="flex-initial text-gray-700 text-center  px-0 py-0 m-4 ml-0">
-                    <Autocomplete
-                        freeSolo
-                        options={options}
-                        className={classes.searchControl}
-                        getOptionLabel={option => option.display_name}
-                        open={open}
-                        onOpen={() => {
-                            setOpen(true);
-                        }}
-                        onClose={() => {
-                            setOpen(false);
-                        }}
-                        autoComplete
-                        loading={loading}
-                        inputValue={inputValue}
-                        includeInputInList
-                        disableOpenOnFocus
-                        onChange={(event, newValue) => console.log(newValue)}
-                        renderInput={params => (
-                            <TextField
-                                {...params}
-                                label=""
-                                variant="outlined"
-                                onChange={event => handleChange(event.target.value)}
-                                fullWidth
-                            />
-                        )}
-                        renderOption={option => {
-                            return <div>{option.display_name}</div>;
-                        }}
-                    />
+                <div class="flex-initial text-gray-700 text-center px-0 py-0 m-4 ml-0">
+                    <div class="relative text-gray-600">
+                        <input type="search" name="serch" placeholder="Search"
+                            class="bg-white h-10 w-full px-32 pr-48 rounded-full text-sm focus:outline-none" />
+                            <select
+                                className='absolute outline-none rounded-full p-2 mt-1 mr-1 left-0 top-0'
+                                value={unit}
+                                onChange={(event, value) => setUnit(value)}
+                            >
+                                <option selected value={'http://localhost:8080/api/v1/search'}>Clothing</option>
+                                <option value={2}>Electronics</option>
+                            </select>
+                        
+                        <button type="submit" class="bg-red-600 absolute rounded-full right-0 top-0 p-2 mt-1 mr-1">
+                            <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+                                version="1.1" id="Capa_1" x="0px" y="0px"
+                                viewBox="0 0 56.966 56.966" style={{ enableBackground: "new 0 0 56.966 56.966", color: '#fff' }}
+                                width="512px" height="512px">
+                                <path
+                                    d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
     );
 }
-
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const top100Films = [
-    { title: 'The Shawshank Redemption', year: 1994 }
-];
