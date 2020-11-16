92cd6f79b5fc62f8ac435d15ab612b819b4bc39e
A00079 <bajay0007@gmail.com>
2020-11-16 15:50:01 +0530

    home page

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,9 +1,19 @@
+import React from 'react';
 
 const LandingPage = () => {
 
     return (
-        <div>
-         Landing page content goes here
+        <div class="container mx-auto px-4">
+            <section class="py-4 px-4">
+                <div class="flex flex-wrap -mx-4">
+                    <div class="md:w-1/2 px-4 mb-8 md:mb-0">
+                        <img class="rounded shadow-md w-full h-56" src="static/images/clothing.jpg" alt="" />
+                    </div>
+                    <div class="md:w-1/2 px-4 mb-8 md:mb-0">
+                        <img class="rounded shadow-md w-full h-56" src="static/images/electronic.jpg" alt="" />
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
