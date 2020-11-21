933bccb6a144f1a022a2626e125c8d2752a501a2
A00079 <bajay0007@gmail.com>
2020-11-22 01:32:41 +0530

    bug fix

--- client/src/pages/DataYugePages/ProductPage/ProductPage.js
+++ client/src/pages/DataYugePages/ProductPage/ProductPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ProductSpecs } from "./components";
+import { ProductSpecs, ProductImgBanner } from "./components";
 const ProductPage = (props) => {
     console.log(props)
 
