fa899acfd89cf27b235b493eff0b8b088eae006e
A00079 <bajay0007@gmail.com>
2020-11-24 03:50:19 +0530

    Added stores

--- client/src/pages/DataYugePages/ProductsPage/components/ComparisonPopUp/WebsiteThumbSlider.js
+++ client/src/pages/DataYugePages/ProductsPage/components/ComparisonPopUp/WebsiteThumbSlider.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import SwiperCore, { Navigation, Pagination, Autoplay, Thumbs } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import { Text } from '../../../../../components';
 import "swiper/swiper.scss";
 import "swiper/components/navigation/navigation.scss";
 import "swiper/components/pagination/pagination.scss";
@@ -12,22 +12,59 @@ const WebsiteThumbSlider = ({ stores }) => {
 	console.log(stores);
 	const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
+	// 	flipkart:
+	// is_cod: "1"
+	// is_emi: "0"
+	// product_color: "white"
+	// product_delivery: "3-5"
+	// product_delivery_cost: "0"
+	// product_mrp: "3240"
+	// product_offer: ""
+	// product_price: "2499"
+	// product_store: "Flipkart"
+	// product_store_logo: "https://images-api.datayuge.in/image/ZmxpcGthcnRfc3RvcmUucG5n.png"
+	// product_store_url: "https://dl.flipkart.com/dl/orient-wendy-3-blade-ceiling-fan/p/itmdwufxecghveg5?pid=FANEFKJZFVHYEMEX&affid=arunbabul"
+	// return_time: "10 Days"
 	return (
 		<div>
 			<Swiper thumbs={{ swiper: thumbsSwiper }}>
-				<React.Fragment>
-					<SwiperSlide>
-						<div>test ajayyy active</div>
-					</SwiperSlide>
-					<SwiperSlide>
-						<div>test sagar active</div>
-					</SwiperSlide>
-					<SwiperSlide>
-						<div>test epic acrtbve</div>
-					</SwiperSlide>
-				</React.Fragment>
+				{stores.map((el, index) =>
+					Object.keys(el).map((elem) => (
+						<React.Fragment key={index}>
+							<SwiperSlide>
+								<div className='p-2'>
+									<div className="flex justify-between">
+										<Text variant="primaryDark" size="lg">{el[elem].product_store}</Text>
+										<div>
+											<Text variant="primaryDark" size="sm">{el[elem].is_cod === '1' ? 'Cash on delivery available' : ""}</Text>
+											<Text variant="primaryDark" classes="block text-right" size="sm">{el[elem].is_emi === '1' ? 'EMI available' : ""}</Text>
+										</div>
+									</div>
+									<div className='flex space-x-6'>
+										&#8377; &nbsp; <Text variant="primary" size="md">{el[elem].product_price}</Text>
+										<del>
+											&#8377;&nbsp; <Text variant="primary" size="md">{el[elem].product_mrp}</Text>
+										</del>
+									</div>
+									<div className='flex space-x-6 items-center'>
+										<Text variant="primary" size="md">Available colours</Text>
+										<div className={`h-6 w-6 rounded-full shadow-xl`} style={{ backgroundColor: el[elem].product_color }}>
+										</div>
+									</div>
+									<div className='flex space-x-6'>
+										<Text variant="primary" size="md">Product Delivery: {el[elem].product_delivery}</Text>
+										<Text variant="primary" size="md">Product Delivery Cost: {el[elem].product_delivery_cost}</Text>
+									</div>
+									<div className='flex space-x-6'>
+										<Text variant="primary" size="md">Return Time: {el[elem].return_time}</Text>
+										<Text variant="primary" size="md">Product Offer: {el[elem].product_offer}</Text>
+									</div>
+								</div>
+							</SwiperSlide>
+						</React.Fragment>
+					)),
+				)}
 			</Swiper>
-
 			<Swiper
 				onSwiper={setThumbsSwiper}
 				spaceBetween={10}
