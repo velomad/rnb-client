62121129c6b4547ca317aadaa580b948583b8fef
velomad <48247308+velomad@users.noreply.github.com>
2020-12-02 15:37:01 +0530

    enhanced search slider

--- client/src/pages/DataYugePages/ProductPage/components/LowPriceDetector/Detector.js
+++ client/src/pages/DataYugePages/ProductPage/components/LowPriceDetector/Detector.js
@@ -12,7 +12,7 @@ const Detector = (props) => {
 				<div className={`${props.index == 0 ? "" : ""}  outline-black p-2 `}>
 					{/* <span class="animate-ping relative inline-flex h-full w-full rounded-full bg-purple-400 opacity-75"></span> */}
 					{props.index == 0 && (
-						<span class=" absolute animate-ping right-4 transform -translate-y-4 inline-flex rounded-full h-3 w-3 pb-8 bg-pink-500"></span>
+						<span class=" absolute animate-ping  right-4 transform -translate-y-4 inline-flex rounded-full h-3 w-3 pb-8 bg-pink-500"></span>
 					)}
 					<Text weight="600" variant="primary">
 						&#8377; {props.price}
--- client/src/pages/MobileProductsPage/components/FiltersPopUp/GenderFilter.js
+++ client/src/pages/MobileProductsPage/components/FiltersPopUp/GenderFilter.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Text } from "../../../../components";
 
 const GenderFilter = (props) => {
 	const [selectedGender, setSelectedGender] = useState("");
@@ -24,35 +25,41 @@ const GenderFilter = (props) => {
 
 	return (
 		<div>
-			<ul className="p-4 py-4 space-y-6">
+			<ul className="p-4 space-y-2">
 				<li onClick={() => handleGenderSelection("men")}>
 					<div
 						className={
 							selectedGender === "men" || selectedFilter === "men"
-								? "flex items-center space-x-6 rounded text-white bg-pink-500 transition duration-500 ease-in-out"
+								? "flex items-center space-x-6 rounded text-white bg-gray-900 transition duration-500 ease-in-out"
 								: "flex items-center space-x-6"
 						}
 					>
 						{/* <div>
 							<img src="/static/images/male.png" width="50" />
 						</div> */}
-						<div
-						
-						className="p-4">MEN</div>
+						<div className="p-4">
+							<Text size="lg" classes="capitalize" isTitle={true}>
+								men
+							</Text>
+						</div>
 					</div>
 				</li>
 				<li onClick={() => handleGenderSelection("women")}>
 					<div
 						className={
 							selectedGender === "women" || selectedFilter === "women"
-								? "flex items-center space-x-6 rounded text-white bg-pink-500 transition duration-500 ease-in-out"
+								? "flex items-center space-x-6 rounded text-white bg-gray-900 transition duration-500 ease-in-out"
 								: "flex items-center space-x-6"
 						}
 					>
 						{/* <div>
 							<img src="/static/images/female.png" width="50" />
 						</div> */}
-						<div className="p-4">WOMEN</div>
+						<div className="p-4">
+							<Text size="lg" classes="capitalize" isTitle={true}>
+								women
+							</Text>
+						</div>
 					</div>
 				</li>
 			</ul>
