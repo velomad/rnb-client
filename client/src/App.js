421ae57e045beff5de8eb6b1448d0387c78f3597
velomad <sagaryc111@gmail.com>
2021-01-21 15:07:11 +0530

    added google analytics

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import { Provider } from "react-redux";
 import store from "./store";
-
+import ReactGA from "react-ga";
 import Routes from "./Routes";
 import "./assets/scss/index.scss";
 import history from "./utils/history";
 import { useState, useEffect } from "react";
 
 const App = () => {
-	const [test, setTest] = useState("");
+  const [test, setTest] = useState("");
 
-	history.listen((location) => {
-		setTest(location.search);
-	});
+  history.listen((location) => {
+    setTest(location.search);
+  });
 
-	useEffect(() => {
-		console.log("Listning...");
-	}, [test]);
+  const initGA = () => {
+    ReactGA.initialize("G-J704V5JT61");
+  };
 
-	return (
-		<Provider store={store}>
-			<Routes />
-		</Provider>
-	);
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname);
+    initGA();
+    console.log("Listning...");
+  }, [test]);
+  // G-J704V5JT61
+
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  );
 };
 export default App;
--- client/src/layouts/PublicLayout/components/Navbar/components/Menu/DynamicContent/Dynamic.js
+++ client/src/layouts/PublicLayout/components/Navbar/components/Menu/DynamicContent/Dynamic.js
@@ -10,157 +10,157 @@ import { history } from "../../../../../../../utils";
 import { Text } from "../../../../../../../components";
 
 const DynamicContent = (props) => {
-	const [webCategoriesMen, setWebCategoriesMen] = React.useState([]);
-	const [webCategoriesWomen, setWebCategoriesWomen] = React.useState([]);
+  const [webCategoriesMen, setWebCategoriesMen] = React.useState([]);
+  const [webCategoriesWomen, setWebCategoriesWomen] = React.useState([]);
 
-	const setMenuItems = (activate) => {
-		props.onGetMenu(activate);
-	};
+  const setMenuItems = (activate) => {
+    props.onGetMenu(activate);
+  };
 
-	const handleCategoryClick = (categoryName) => {
-		props.hideDropDown();
-		history.push(
-			`/products?website=${props.currentBrand}&category=` + categoryName,
-		);
-	};
-	React.useEffect(() => {
-		if (props.currentBrand == "amazon") {
-			console.log("amazonmen", amazonmen);
-			console.log("amazonwomen", amazonwomen);
-			setWebCategoriesMen(amazonmen);
-			setWebCategoriesWomen(amazonwomen);
-		} else if (props.currentBrand == "flipkart") {
-			setWebCategoriesMen(men);
-			setWebCategoriesWomen(women);
-		} else if (props.currentBrand == "myntra") {
-			setWebCategoriesMen(myntramen);
-			setWebCategoriesWomen(myntrawomen);
-		} else if (props.currentBrand == "ajio") {
-			setWebCategoriesMen(ajiomen);
-			setWebCategoriesWomen(ajiowomen);
-		} else if (props.currentBrand == "tatacliq") {
-			setWebCategoriesMen(tatacliqmen);
-			setWebCategoriesWomen(tatacliqwomen);
-		} else if (props.currentBrand == "bewakoof") {
-			setWebCategoriesMen(bewakoofmen);
-			setWebCategoriesWomen(bewakoofwomen);
-		} else if (props.currentBrand == "snapdeal") {
-			setWebCategoriesMen(snapdealmen);
-			setWebCategoriesWomen(snapdealwomen);
-		}
-	}, [props]);
-	return (
-		<React.Fragment>
-			{props.categories.map((el, index) => (
-				<section
-					key={el}
-					className=" opacity-100 relative text-gray-700 font-light font-light border-b px-4 pb-4 md:py-3 w-full md:border-none md:w-64"
-				>
-					<div
-						class="md:hidden"
-						onClick={() => {
-							setMenuItems(el);
-						}}
-					>
-						<div class="grid grid-cols-2 gap-56 ">
-							<div className="flex items-center">
-								<Text
-									classes="capitalize text-pink-700 focus:outline-none border-t border-white py-4 w-full text-left"
-									weight="600"
-									type="button"
-								>
-									{props.activePanel === "men"
-										? el.split("-")[1]
-										: el.split("-")[1]}
-								</Text>
-							</div>
-							<div className="pt-3">
-								<svg
-									className="w-6 h-6"
-									fill="none"
-									stroke="currentColor"
-									viewBox="0 0 24 24"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth={2}
-										d="M19 9l-7 7-7-7"
-									/>
-								</svg>
-							</div>
-						</div>
-					</div>
-					<div>
-						<a
-							class="uppercase text-xs font-bold tracking-wider text-pink-700 hidden md:block"
-							href="#"
-						>
-							<div className="flex items-center">
-								{/* <img
+  const handleCategoryClick = (categoryName) => {
+    props.hideDropDown();
+    history.push(
+      `/products?website=${props.currentBrand}&category=` + categoryName
+    );
+  };
+  React.useEffect(() => {
+    if (props.currentBrand == "amazon") {
+      console.log("amazonmen", amazonmen);
+      console.log("amazonwomen", amazonwomen);
+      setWebCategoriesMen(amazonmen);
+      setWebCategoriesWomen(amazonwomen);
+    } else if (props.currentBrand == "flipkart") {
+      setWebCategoriesMen(men);
+      setWebCategoriesWomen(women);
+    } else if (props.currentBrand == "myntra") {
+      setWebCategoriesMen(myntramen);
+      setWebCategoriesWomen(myntrawomen);
+    } else if (props.currentBrand == "ajio") {
+      setWebCategoriesMen(ajiomen);
+      setWebCategoriesWomen(ajiowomen);
+    } else if (props.currentBrand == "tatacliq") {
+      setWebCategoriesMen(tatacliqmen);
+      setWebCategoriesWomen(tatacliqwomen);
+    } else if (props.currentBrand == "bewakoof") {
+      setWebCategoriesMen(bewakoofmen);
+      setWebCategoriesWomen(bewakoofwomen);
+    } else if (props.currentBrand == "snapdeal") {
+      setWebCategoriesMen(snapdealmen);
+      setWebCategoriesWomen(snapdealwomen);
+    }
+  }, [props]);
+  return (
+    <React.Fragment>
+      {props.categories.map((el, index) => (
+        <section
+          key={el}
+          className=" opacity-100 relative text-gray-700 font-light font-light border-b px-4 pb-4 md:py-3 w-full md:border-none md:w-64"
+        >
+          <div
+            class="md:hidden"
+            onClick={() => {
+              setMenuItems(el);
+            }}
+          >
+            <div class="grid grid-cols-2 gap-56 ">
+              <div className="flex items-center">
+                <Text
+                  classes="capitalize text-pink-700 focus:outline-none border-t border-white py-4 w-full text-left"
+                  weight="600"
+                  type="button"
+                >
+                  {props.activePanel === "men"
+                    ? el.split("-")[1]
+                    : el.split("-")[1]}
+                </Text>
+              </div>
+              <div className="pt-3">
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M19 9l-7 7-7-7"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div>
+            <a
+              class="uppercase text-xs font-bold tracking-wider text-pink-700 hidden md:block"
+              href="#"
+            >
+              <div className="flex items-center">
+                {/* <img
 								src={`/static/images/${el.split('-')[1]}.png`}
 								width="50px"
 								className="mr-1"
 							/> */}
-								<p className="font-semibold">{el.split("-")[1]}</p>
-							</div>
-						</a>
-						<article
-							className={
-								props.activatesmallPanel === el
-									? "md:h-auto -mt-4 md:mt-0 overflow-hidden transition duration-700"
-									: "h-0 md:h-auto -mt-4 md:mt-0 overflow-hidden"
-							}
-						>
-							<ul class="my-5 text-sm tracking-wide">
-								{props.activePanel === "men" && !!webCategoriesMen[el]
-									? webCategoriesMen[el].map((el, index) => (
-											<li
-												key={index + "men"}
-												onClick={() => handleCategoryClick(el)}
-												class="my-3 cursor-pointer tracking-wide capitalize"
-											>
-												{/* {props.currentBrand == 'myntra'?el.split('-')[1]:props.currentBrand == 'tatacliq' || 'bewakoof' ?el.split('-men')[0].replace('-',' '):el} */}
-												<Text>
-													{props.currentBrand == "myntra"
-														? el.split("-").join(" ")
-														: props.currentBrand == "tatacliq" || "bewakoof"
-														? el.split("-").join(" ")
-														: el.split("-").join(" ")}
-												</Text>
-											</li>
-									  ))
-									: null}
-								{props.activePanel !== "men" && !!webCategoriesWomen[el]
-									? webCategoriesWomen[el].map((el, index) => (
-											<li
-												key={index + "women"}
-												onClick={() => handleCategoryClick(el)}
-												class="my-3 cursor-pointer tracking-wide capitalize"
-											>
-												{/* {props.currentBrand == "myntra"
+                <p className="font-semibold">{el.split("-")[1]}</p>
+              </div>
+            </a>
+            <article
+              className={
+                props.activatesmallPanel === el
+                  ? "md:h-auto -mt-4 md:mt-0 overflow-hidden transition duration-700"
+                  : "h-0 md:h-auto -mt-4 md:mt-0 overflow-hidden"
+              }
+            >
+              <ul class="my-5 text-sm tracking-wide">
+                {props.activePanel === "men" && !!webCategoriesMen[el]
+                  ? webCategoriesMen[el].map((el, index) => (
+                      <li
+                        key={index + "men"}
+                        onClick={() => handleCategoryClick(el)}
+                        class="my-3 cursor-pointer tracking-wide capitalize"
+                      >
+                        {/* {props.currentBrand == 'myntra'?el.split('-')[1]:props.currentBrand == 'tatacliq' || 'bewakoof' ?el.split('-men')[0].replace('-',' '):el} */}
+                        <Text>
+                          {props.currentBrand == "myntra"
+                            ? el.split("-").join(" ")
+                            : props.currentBrand == "tatacliq" || "bewakoof"
+                            ? el.split("-").join(" ")
+                            : el.split("-").join(" ")}
+                        </Text>
+                      </li>
+                    ))
+                  : null}
+                {props.activePanel !== "men" && !!webCategoriesWomen[el]
+                  ? webCategoriesWomen[el].map((el, index) => (
+                      <li
+                        key={index + "women"}
+                        onClick={() => handleCategoryClick(el)}
+                        class="my-3 cursor-pointer tracking-wide capitalize"
+                      >
+                        {/* {props.currentBrand == "myntra"
 												? el.split("-")[1]
 												: props.currentBrand == "tatacliq" || "bewakoof"
 												? el.split("-women")[0].replace("-", " ")
 												: el} */}
-												<Text>
-													{props.currentBrand == "myntra"
-														? el.split("-").join(" ")
-														: props.currentBrand == "tatacliq" || "bewakoof"
-														? el.split("-").join(" ")
-														: el.split("-").join(" ")}
-												</Text>
-											</li>
-									  ))
-									: null}
-							</ul>
-						</article>
-					</div>
-				</section>
-			))}
-		</React.Fragment>
-	);
+                        <Text>
+                          {props.currentBrand == "myntra"
+                            ? el.split("-").join(" ")
+                            : props.currentBrand == "tatacliq" || "bewakoof"
+                            ? el.split("-").join(" ")
+                            : el.split("-").join(" ")}
+                        </Text>
+                      </li>
+                    ))
+                  : null}
+              </ul>
+            </article>
+          </div>
+        </section>
+      ))}
+    </React.Fragment>
+  );
 };
 
 export default DynamicContent;
